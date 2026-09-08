2b43cdfda29d5cee7a06ae5dc2409fefcf6841c6
System administrator <chieho.c@tutanota.com>
2026-09-08 20:29:18 +0200

    add nur-community

--- flake.nix
+++ flake.nix
@@ -2,24 +2,35 @@
   description = "Meine NixOS Flake Konfiguration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; # Oder Ihre Version, z.B. "github:nixos/nixpkgs/nixos-24.11"
+   
+    #unstable nixpkgs
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable"; 
     
+    #home-manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+ 
+    #nur-community
+    nur = {
+      url = "github:nix-community/NUR";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    
   };
 
-  outputs = { self, nixpkgs, home-manager ,... }@inputs: {
+  outputs = { self, nixpkgs, home-manager , nur,... }@inputs: {
     nixosConfigurations = {
       nixos = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux"; # Falls Sie eine andere Architektur nutzen, hier anpassen
+        system = "x86_64-linux"; 
         modules = [
           ./hardware-configuration.nix
           ./configuration.nix
 	  ./jupyter.nix
 	  home-manager.nixosModules.home-manager
 	  {
+            nixpkgs.overlays = [ nur.overlays.default ];
 	    home-manager.useGlobalPkgs = true;	  
 	    home-manager.useUserPackages = true;	   
 	    home-manager.users.chieho = import ./home.nix;
